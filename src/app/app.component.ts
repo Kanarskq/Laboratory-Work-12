aef3b3d7cff58f4f152f169de66d15661a50891c
Kanarskq <anatoliykanarskiy075@gmail.com>
2023-11-28 19:52:35 +0100

    Laboratory work 12

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,4 +11,36 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'lab11';
+
+  products: any[] = []
+  sortedProducts: any[] = [];
+  sortDirection: { [key: string]: string } = {};
+
+  public ngOnInit(){
+    fetch('https://fakestoreapi.com/products')
+            .then(res=>res.json())
+            .then(json=> {
+              this.products = json;
+              this.sortedProducts = [...this.products];
+            });
+  }
+  
+  public sortProducts(parameter: string) {
+    this.sortDirection[parameter] = this.sortDirection[parameter] === 'asc' ? 'desc' : 'asc';
+
+    this.sortedProducts.sort((a, b) => {
+      const modifier = this.sortDirection[parameter] === 'asc' ? 1 : -1;
+
+      switch (parameter) {
+        case 'price':
+          return (a.price - b.price) * modifier;
+        case 'name':
+          return a.title.localeCompare(b.title) * modifier;
+        case 'rating':
+          return (b.rating.rate - a.rating.rate) * modifier;
+        default:
+          return 0;
+      }
+    });
+  }
 }
